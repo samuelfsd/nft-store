fcedd5cbe1d02caa50a0bf79969017f63a20f765
Samuel <samuel.estudo778@gmail.com>
2023-04-21 14:50:49 -0300

    feat: create styles filters section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,24 @@
+import styles from "./styles.module.css";
+
 export function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.filters}>
+          <button className={styles.button}>Filters</button>
+          <button className={styles.button}>NFT's</button>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Search by collection, user or NFT..."
+          />
+          <button className={styles.button}>Trending</button>
+          <button className={styles.smallButton}>G</button>
+          <button className={styles.smallButton}>L</button>
+        </div>
+
+        <div className={styles.nfts}></div>
+      </div>
+    </div>
+  );
 }
